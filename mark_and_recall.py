9f8a27d4d685f19d31d790f73b7871100838bf71
jedi <amajid@nodeflux.io>
2019-05-12 01:59:04 +0700

    remove and separate function call into tutorial.py

--- mark_and_recall.py
+++ mark_and_recall.py
@@ -13,45 +13,47 @@ ori_path = Path.cwd() / 'data' / 'Replay-Attack' / 'ORI'
 
 
 class CreatePytorchDatasetFormat:
-	def __init__(self, dataset_path: str):
-		self.dataset_path = dataset_path
-	def _read_images(self):
-		if self.dataset_path.exists():
-			p = Path(self.dataset_path)
+	
+	def __init__(self, origin: str):
+		self.origin = origin
+		
+	def _read_origin(self):
+		if self.origin.exists():
+			p = Path(self.origin)
 			for path in p.rglob("*.jpg"):
 				yield path
 		else:
 			raise ValueError("Directory does not exist")
 
-	def _create_destination_path(self, levels: int):
+	def show_origin(self):
 		"""
-		Private method for destination_path and rename_and_move_images
+		simulate the images path you're inputting
 		"""
-		for image_path in self._read_images():
+		for image_path in self._read_origin():
+			print(image_path)
+
+	def simulate_mark_and_recall(self, levels: int):
+		"""
+		simulate the destination path of your image
+		"""
+		for destination_path, new_name in self.mark(levels):
+			print("Mark:", destination_path / new_name)
+
+	def mark(self, levels: int):
+		"""
+		Mark destination
+		"""
+		for image_path in self._read_origin():
 			destination_path = image_path.parents[levels-1].resolve()
 			parts = image_path.parts[-levels:]
 			new_name = ''.join(str(part) for part in parts)
 			yield destination_path, new_name
 
-	def show_source_path(self):
-		"""
-		simulate the images path you're inputting
-		"""
-		for image_path in self._read_images():
-			print(image_path)
-
-	def simulate_relocation(self, levels: int):
-		"""
-		simulate the destination path of your image
-		"""
-		for destination_path, new_name in self._create_destination_path(levels):
-			print(destination_path / new_name)
-
-	def relocate_images(self, levels: int, verbose=False):
+	def recall(self, levels: int, verbose=False):
 		"""
 		levels correspond to how many levels you want to move up
 		"""
-		for image_path, (destination_path, new_name) in zip(self._read_images(), self._create_destination_path(levels)):
+		for image_path, (destination_path, new_name) in zip(self._read_origin(), self.mark(levels)):
 			image_path.replace(destination_path / new_name)
 			if verbose:
 				print(image_path, "is moved to", destination_path / new_name)
@@ -63,7 +65,7 @@ class PartitionPytorchDataset(CreatePytorchDatasetFormat):
 	"""
 	def __init__(self, dataset_path: str, labels: List, training_size: float, test_size:float, random_state: int):
 		super().__init__
-		self.dataset_path = dataset_path
+		self.origin = dataset_path
 		self.labels = labels
 		self.training_size = training_size
 		self.test_size = test_size
@@ -84,7 +86,7 @@ class PartitionPytorchDataset(CreatePytorchDatasetFormat):
 		"""
 		target = []
 		name = []
-		for image_path in self._read_images():
+		for image_path in self._read_origin():
 			for label in self._encode_labels():
 				if re.search(label, str(image_path)):
 					# breakpoint()
@@ -111,7 +113,7 @@ class PartitionPytorchDataset(CreatePytorchDatasetFormat):
 
 		return xTrain,yTrain,xValidation,yValidation,xTest,yTest
 
-	def _create_destination_path(self, levels):
+	def mark(self, levels):
 		"""
 		private method to generate destination path. 
 		Levels correspond to how much you want to move up in 
@@ -135,48 +137,21 @@ class PartitionPytorchDataset(CreatePytorchDatasetFormat):
 					yield (Path(destination_path), Path(destination_path).parents[levels] / 'test' / label / Path(destination_path).name)
 
 
-	def simulate_relocation(self, levels: int):
+	def simulate_mark_and_recall(self, levels: int):
 		"""
-		run this to simulate the relocation.
+		run this to simulate the mark and recall.
 		level denotes how much you want to move up in the directory 
 		"""
-		for destination_path in self._create_destination_path(levels):
-			print(destination_path[0], "would be moved to", destination_path[1])
+		for destination_path in self.mark(levels):
+			print("mark: "destination_path[0], /n "recall;", destination_path[1])
 		
-	def relocate(self, levels: int):
-		for destination_path in self._create_destination_path(levels):
+	def recall(self, levels: int):
+		for destination_path in self.mark(levels):
 			origin = destination_path[0]
 			destination = destination_path[1]
 			# breakpoint()
 			Path(destination).parents[0].mkdir(parents=True, exist_ok=True)
 			destination_path[0].replace(destination_path[1])
-
-
-		
-
-			
-
-
-
-
-
-
-# a = CreatePytorchDatasetFormat(attack_path)
-# a.show_source_path()
-#a.simulate_destination_path(3)
-#a.rename_and_move_images(3)
-
-b = PartitionPytorchDataset(ori_path, ["attack", "real"], 0.7, 0.3, 69)
-# b.simulate_relocation(1)
-b.relocate(1)
-# a,b,c,d,e,f = b._stratify_sampling()
-# print(e)
-# labels = b.encode_labels(["attack", "real"])
-
-# #print(labels)
-
-# #b.show_source_path()
-# b.simulate_destination_path(labels, 0.7, 0.3, 69, 1)
 
 
 
