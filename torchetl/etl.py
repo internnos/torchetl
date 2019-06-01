3e3cb287c3719f29b0b69affb6baa47ae2b4fd9d
jedi <amajid@nodeflux.io>
2019-06-02 03:53:15 +0700

    add feature to do random sampling instead of stratify sampling only

--- torchetl/etl.py
+++ torchetl/etl.py
@@ -6,7 +6,7 @@ from typing import List, Tuple, Callable
 
 # external package
 import numpy as np
-from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import StratifiedShuffleSplit, train_test_split
 import torch
 from torchvision import transforms, datasets
 from torch.utils.data import Dataset
@@ -16,11 +16,13 @@ import cv2
 # user
 from torchetl.base.dataset import BaseDataset
 
+
+
 class Extract(BaseDataset):
 	def __init__(self, parent_directory: str, 
 				 extension: str,
 				 labels: List[str], 
-				 training_size: float, 
+				 train_size: float, 
 				 random_state: int, 
 				 verbose: bool) -> None:
 		"""Class for creating csv files of train, validation, and test
@@ -39,8 +41,8 @@ class Extract(BaseDataset):
 		"""
 		super().__init__(parent_directory, extension)
 		self.labels = labels
-		self.training_size = training_size
-		self.test_size = 1 - training_size
+		self.train_size = train_size
+		self.test_size = 1 - train_size
 		self.random_state = random_state
 		self.verbose = verbose
 
@@ -82,7 +84,7 @@ class Extract(BaseDataset):
 		Tuple of train(X, y), validation(X, y), and test(X, y)	
 		"""
 		x, y = self._create_dataset_array()
-		sss = StratifiedShuffleSplit(n_splits=1, train_size=self.training_size, test_size=self.test_size, random_state=self.random_state)
+		sss = StratifiedShuffleSplit(n_splits=1, train_size=self.train_size, test_size=self.test_size, random_state=self.random_state)
 
 		for train_index, validation_test_index in sss.split(x, y):
 			x_train, x_validation_test = x[train_index], x[validation_test_index]
@@ -101,7 +103,30 @@ class Extract(BaseDataset):
 			print("Finished splitting dataset into train, validation, and test")
 		return train, validation, test
 
-	def extract(self, file_prefix: str, save_path: str):
+	def _random_sampling(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+		x, y = self._create_dataset_array()
+		x_train, x_validation_test, y_train, y_validation_test = train_test_split(x, 
+																				  y, 
+																				  train_size = self.train_size,
+																				  test_size = self.test_size, 
+																				  random_state=self.random_state)
+
+		x_validation, x_test, y_validation, y_test = train_test_split(x_validation_test,
+																	  y_validation_test,
+																	  train_size = 0.5,
+																	  test_size = 0.5,
+																	  random_state = self.random_state)
+
+		train = np.c_[x_train, y_train]
+		validation = np.c_[x_validation, y_validation]
+		test = np.c_[x_test, y_test]
+
+		if self.verbose:
+			print("Finished splitting dataset into train, validation, and test")
+		return train, validation, test
+
+
+	def extract(self, file_prefix: str, save_path: str, is_random_sampling: bool):
 		"""Create csv file of train, validation, and test
 
 		Parameters
@@ -111,13 +136,18 @@ class Extract(BaseDataset):
 			Have the format of file_prefix_train.csv, file_prefix_validation.csv, and test_validation.csv
 		save_path
 			The parent_directory folder name of file_prefix_train.csv, file_prefix_validation.csv, and test_validation.csv
-
+		is_random_sampling
+			extract train, validation, and test based on random sampling. If set to false, then stratify sampling is applied.
+			The best practice is to use stratify sampling for classification tasks and random sampling for regression tasks
 		Returns
 		-------
 		train, validation, and test csv with the following name:
 		file_prefix_train.csv, file_prefix_validation.csv, and test_validation.csv	
 		"""
-		train, validation, test = self._stratify_sampling()
+		if is_random_sampling:
+			train, validation, test = self._random_sampling()
+		else:
+			train, validation, test = self._stratify_sampling()
 
 		save_into = Path.cwd() / save_path
 		save_into.mkdir(parents=True, exist_ok=True)
