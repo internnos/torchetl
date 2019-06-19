efecc10c6d47ce7a739c21ea08d659fb6ef844f8
jedi <amajid@nodeflux.io>
2019-06-19 23:09:43 +0700

    squash bugs in Extract._create_dataset_array

--- tutorial.py
+++ tutorial.py
@@ -2,14 +2,14 @@ from torchetl.etl import Extract
 from pathlib import Path
 import pandas as pd
 
-parent_directory = Path.cwd() / 'data'
+parent_directory = 'data'
 
 combined_dataset = Extract(parent_directory = parent_directory, 
-              extension = 'jpg', 
-              labels = ['attack', 'real'], 
-              train_size = 0.8,
-              random_state = 69,
-              verbose = True,
-            )
-import pdb; pdb.set_trace()
-combined_dataset._create_dataset_array()
+    extension = 'jpg', 
+    labels = ['attack', 'real'], 
+    train_size = 0.8,
+    random_state = 69,
+    verbose = True
+)
+
+combined_dataset.extract(file_prefix='exp', save_path='data', is_random_sampling=False)
